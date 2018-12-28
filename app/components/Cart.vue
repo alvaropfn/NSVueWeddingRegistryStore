977b1669f21399d274f99b719d6d4381d1ab3ef4
alvaropfn <alvaropfn@gmail.com>
2018-12-28 09:22:32 -0300

    sucessfully integrated cart

--- app/components/Cart.vue
+++ app/components/Cart.vue
@@ -5,24 +5,58 @@
         <Button dock="left" class="btn btn-primary" text="back" @tap="$navigateBack"></Button>
         <StackLayout>
           <Label text="Wedding Companion Store"/>
-          <Label text="wallet"/>
+          <Label :text="this.$props.wallet"/>
         </StackLayout>
-        <Button dock="right" class="btn btn-primary" text="cart" @tap="goToCart"></Button>
+        <Button dock="right" class="btn btn-primary" text="Pay" @tap="payCart"></Button>
       </DockLayout >
     </ActionBar>
-    <StackLayout>
-    </StackLayout>
+    <GridLayout rows="*" columns="*">
+      <ListView row="0" class="list-group" for="item in cart" > <!--@itemTap="select"-->
+        <v-template>
+          <StackLayout class="list-group-item">
+            <Image :src="item.src" class="thumb"  @tap="showDetails(item)" ></Image>
+            <DockLayout  width="320">
+              <Label class="item__title" dock="left"  :text="item.name"></Label>
+              <TextView dock="rigth" width="100" editable="false">
+                <FormattedString  >
+                    <Span text="quantity:" />
+                    <Span :text="item.quantity"/>
+                </FormattedString>
+              </TextView>
+            </DockLayout>
+            <DockLayout  width="320">
+              <TextView dock="left" width="100" editable="false">
+                <FormattedString  >
+                    <Span text="price:" />
+                    <Span :text="item.price"/>
+                </FormattedString>
+              </TextView>
+              <Button class="btn btn-secundary btn-secundary--add" dock="rigth" text="-" width="100"  @tap="remToCart(item)" :style="item.quantity > 0 ? isEnable='true': isEnable='false'"></Button>
+            </DockLayout>
+
+          </StackLayout>
+z
+        </v-template>
+      </ListView>
+    </GridLayout>
   </Page>
 </template>
 
 <script>
 export default {
-  props: ["cart"],
+  props: ["cart", "wallet"],
+  computed:{
+    appName: "Wedding Store"
+  },
   methods:{
-    goToCart(){
-      console.log(this.$props.cart.length)
+    payCart(){
+      console.log(this.$props.wallet)
+    },
+    remToCart(){
+      console.log(this.$props.wallet)
     }
-  }
+  },
+  
 };
 
 
